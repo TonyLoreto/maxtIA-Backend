da6669f12e71050356b449936f97a59bd8637730
tonyloreto <carmonacarlos.a.mx@gmail.com>
2024-10-20 11:47:32 -0600

    se cambia pdf por la data más actual

--- api.py
+++ api.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*", "https://9d01-189-204-144-186.ngrok-free.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
